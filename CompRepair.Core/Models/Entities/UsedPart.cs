ca99bf0328508a7f438fcc06fba0824302087529
artemkiruhin <temkaunik@gmail.com>
2025-01-15 23:12:22 +0300

    Used part entity

---
+++ CompRepair.Core/Models/Entities/UsedPart.cs
@@ -0,0 +1,10 @@
+﻿namespace CompRepair.Core.Models.Entities;
+
+public class UsedPart
+{
+    public Guid Id { get; set; }
+    public int Quantity { get; set; }
+    public decimal PricePerUnit { get; set; }
+    
+    public Guid RepairOrderId { get; set; }
+}
