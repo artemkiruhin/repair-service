fe9e1aaaff57b8d850498ccbcc0cb397e384fbb3
artemkiruhin <temkaunik@gmail.com>
2025-01-15 23:14:23 +0300

    Repair order entity created

---
+++ CompRepair.Core/Models/Entities/RepairOrderEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace CompRepair.Core.Models.Entities;
+
+public class RepairOrderEntity
+{
+    public Guid ClientId { get; set; }
+    public Guid TechnicianId { get; set; }
+    public string DeviceType { get; set; }
+    public string SerialNumber { get; set; }
+    public string Problem { get; set; }
+    public string Diagnosis { get; set; }
+    public string Status { get; set; }
+    public decimal EstimatedCost { get; set; }
+    public decimal? FinalCost { get; set; }
+    public DateTime? CompletedAt { get; set; }
+    
+    public ClientEntity Client { get; set; }
+    public UserEntity Technician { get; set; }
+    public ICollection<UsedPart> UsedParts { get; set; }
+}
