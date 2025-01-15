def82e4e09e145cc2816204ed761e0d9b539142d
artemkiruhin <temkaunik@gmail.com>
2025-01-15 23:06:35 +0300

    User entity modified

--- CompRepair.Core/Models/Entities/UserEntity.cs
+++ CompRepair.Core/Models/Entities/UserEntity.cs
@@ -4,6 +4,10 @@ public class UserEntity
 {
     public Guid Id { get; set; }
     public string Username { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public decimal Salary { get; set; }
     public string Password { get; set; } = string.Empty;
     public DateTime HiredDate { get; set; }
     public DateTime? FiredDate { get; set; }
