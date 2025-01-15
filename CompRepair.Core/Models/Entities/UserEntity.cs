a82ae2cb20c8d6b6c3a4117ac28b6268276019fa
artemkiruhin <temkaunik@gmail.com>
2025-01-15 23:04:16 +0300

    User entity

---
+++ CompRepair.Core/Models/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace CompRepair.Core.Models.Entities;
+
+public class UserEntity
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public DateTime HiredDate { get; set; }
+    public DateTime? FiredDate { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Role { get; set; } = string.Empty;
+}
