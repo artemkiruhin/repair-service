f75b97e8b15a856a8b399ec25c8cd542ea68f8fa
artemkiruhin <temkaunik@gmail.com>
2025-01-16 13:14:23 +0300

    Base entity service created

---
+++ CompRepair.Core/Services/EntityServices/Base/BaseEntityService.cs
@@ -0,0 +1,38 @@
+﻿using CompRepair.Core.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompRepair.Core.Services.EntityServices.Base;
+
+public class BaseEntityService<TDto, TEntity> where TEntity : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+    
+    public BaseEntityService()
+    {
+        _context = new AppDbContext();
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        try
+        {
+            await _context.Database.BeginTransactionAsync();
+            
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null) throw new KeyNotFoundException();
+            _dbSet.Remove(entity);
+            var result = await _context.SaveChangesAsync();
+            
+            await _context.Database.CommitTransactionAsync();
+            
+            return result > 0;
+        }
+        catch (Exception e)
+        {
+            await _context.Database.RollbackTransactionAsync();
+            throw;
+        }
+    }
+}
