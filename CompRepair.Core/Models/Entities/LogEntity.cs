0826d898d2faf890fbc119dfc4bf492f5fa845be
artemkiruhin <temkaunik@gmail.com>
2025-01-16 12:10:16 +0300

    Log entity created

---
+++ CompRepair.Core/Models/Entities/LogEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CompRepair.Core.Models.Entities;
+
+public class LogEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid UserId { get; set; }
+    public string Message { get; set; }
+    public string LogType { get; set; }
+}
---
+++ CompRepair.Core/Models/Entities/LogType.cs
@@ -0,0 +1,8 @@
+﻿namespace CompRepair.Core.Models.Entities;
+
+public enum LogType
+{
+    Info,
+    Warn,
+    Error
+}
