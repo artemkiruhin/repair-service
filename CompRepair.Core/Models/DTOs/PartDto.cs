d12e612538dcdbcfee30a9d223bcabf061c6875e
artemkiruhin <temkaunik@gmail.com>
2025-01-16 12:02:40 +0300

    Part dto

---
+++ CompRepair.Core/Models/DTOs/PartDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CompRepair.Core.Models.DTOs;
+
+public class PartDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } 
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public string CategoryName { get; set; }
+    public string ManufacturerName { get; set; }
+    public List<UsedPartDto> UsedParts { get; set; } = [];
+}
