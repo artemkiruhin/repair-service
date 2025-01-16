a0cb110f0f67cd432b2290419f58d31f394a5d06
artemkiruhin <temkaunik@gmail.com>
2025-01-16 12:59:28 +0300

    Sha 256 hasher created

---
+++ CompRepair.Core/Services/Hasher.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CompRepair.Core.Services;
+
+public class Hasher
+{
+    public string Hash(string input) => Convert.ToHexStringLower(SHA256.HashData(Encoding.UTF8.GetBytes(input)));
+}
