b1d4391d73dd5ada9742f5632d599151298e8b2e
artemkiruhin <temkaunik@gmail.com>
2025-01-16 12:07:02 +0300

    Client dto

---
+++ CompRepair.Core/Models/DTOs/ClientDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CompRepair.Core.Models.DTOs;
+
+public class ClientDto
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } 
+    public string Email { get; set; } 
+    public string Phone { get; set; } 
+    public DateTime? BirthDate { get; set; }
+
+    public List<RepairOrderDto> RepairOrders { get; set; } = [];
+}
