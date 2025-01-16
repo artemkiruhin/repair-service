318a0571eaecbc267a7ad6c6f03ee0352e810f59
artemkiruhin <temkaunik@gmail.com>
2025-01-16 12:01:04 +0300

    Used part dto

---
+++ CompRepair.Core/Models/DTOs/UsedPartDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CompRepair.Core.Models.DTOs;
+
+public class UsedPartDto
+{
+    public Guid Id { get; set; }
+    public int Quantity { get; set; }
+    public decimal PricePerUnit { get; set; }
+    public Guid PartId { get; set; }
+    public string PartName { get; set; }
+    public Guid RepairOrderId { get; set; }
+}
