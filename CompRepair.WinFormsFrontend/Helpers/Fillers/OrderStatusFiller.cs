2a7873c2e982882c97c512e2070b174a7aad417c
artemkiruhin <temkaunik@gmail.com>
2025-01-18 17:29:51 +0300

    Order status filler for DGV

---
+++ CompRepair.WinFormsFrontend/Helpers/Fillers/OrderStatusFiller.cs
@@ -0,0 +1,28 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+public class OrderStatusFiller
+{
+    public async Task FillDataGridView(DataGridView dataGridView)
+    {
+        try
+        {
+            var statuses = await new OrderStatusService().GetAllAsync(); 
+
+            dataGridView.Rows.Clear();
+        
+            foreach (var status in statuses)
+            {
+                dataGridView.Rows.Add(
+                    status.Id,
+                    status.Name
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Внутренняя ошибка при заполнени таблицы");
+        }
+    }
+}
