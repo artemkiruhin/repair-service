274af87af860f57160490207690c764df07d5c1f
artemkiruhin <temkaunik@gmail.com>
2025-01-18 17:27:01 +0300

    Device type filler for DGV

---
+++ CompRepair.WinFormsFrontend/Helpers/Fillers/DeviceTypeFiller.cs
@@ -0,0 +1,28 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+public class DeviceTypeFiller
+{
+    public async Task FillDataGridView(DataGridView dataGridView)
+    {
+        try
+        {
+            var types = await new DeviceTypeService().GetAllAsync(); 
+
+            dataGridView.Rows.Clear();
+        
+            foreach (var type in types)
+            {
+                dataGridView.Rows.Add(
+                    type.Id,
+                    type.Name
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Внутренняя ошибка при заполнени таблицы");
+        }
+    }
+}
