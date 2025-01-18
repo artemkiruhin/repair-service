18c3a3002d07a34bcff905eca9f3a93b93a0e3a2
artemkiruhin <temkaunik@gmail.com>
2025-01-18 17:28:37 +0300

    Manufacturer filler for DGV

---
+++ CompRepair.WinFormsFrontend/Helpers/Fillers/ManufacturerFiller.cs
@@ -0,0 +1,28 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+public class ManufacturerFiller
+{
+    public async Task FillDataGridView(DataGridView dataGridView)
+    {
+        try
+        {
+            var manufacturers = await new ManufacturerService().GetAllAsync(); 
+
+            dataGridView.Rows.Clear();
+        
+            foreach (var manufacturer in manufacturers)
+            {
+                dataGridView.Rows.Add(
+                    manufacturer.Id,
+                    manufacturer.Name
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Внутренняя ошибка при заполнени таблицы");
+        }
+    }
+}
