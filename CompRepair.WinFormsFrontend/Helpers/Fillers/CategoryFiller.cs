1fdf136bb1d08699aeb218adb61257261c4313f7
artemkiruhin <temkaunik@gmail.com>
2025-01-18 17:21:49 +0300

    DGV filler for category

---
+++ CompRepair.WinFormsFrontend/Helpers/Fillers/CategoryFiller.cs
@@ -0,0 +1,29 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+public class CategoryFiller
+{
+    public async Task FillDataGridView(DataGridView dataGridView)
+    {
+        try
+        {
+            var categories = await new CategoryService().GetAllAsync(); 
+            
+            dataGridView.Rows.Clear();
+        
+            foreach (var category in categories)
+            {
+                dataGridView.Rows.Add(
+                    category.Id,
+                    category.Name
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Внутренняя ошибка при заполнени таблицы");
+        }
+    }
+
+}
