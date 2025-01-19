1faa4aed61271121a424df3ac9c65c49c041ced6
artemkiruhin <temkaunik@gmail.com>
2025-01-19 09:02:30 +0300

    Part filler for dgv

---
+++ CompRepair.WinFormsFrontend/Helpers/Fillers/PartFiller.cs
@@ -0,0 +1,34 @@
+﻿using CompRepair.Core.Models.DTOs;
+using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+public class PartFiller
+{
+    public async Task FillDataGridView(DataGridView dataGridView)
+    {
+        try
+        {
+            var parts = await new PartService().GetAllAsync(); 
+
+            dataGridView.Rows.Clear();
+        
+            foreach (var part in parts)
+            {
+                dataGridView.Rows.Add(
+                    part.Id,
+                    part.Name,
+                    part.Description,
+                    part.Price,
+                    part.StockQuantity,
+                    part.CategoryName,
+                    part.ManufacturerName
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Внутренняя ошибка при заполнени таблицы");
+        }
+    }
+}
