7ee9ea670ba53f6d346db6b54c6a25ff84e7fc46
artemkiruhin <temkaunik@gmail.com>
2025-01-19 09:15:17 +0300

    Log filler for dgv

---
+++ CompRepair.WinFormsFrontend/Helpers/Fillers/LogFiller.cs
@@ -0,0 +1,31 @@
+﻿using CompRepair.Core.Services;
+
+namespace CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+public class LogFiller
+{
+    public async Task FillDataGridView(DataGridView dataGridView)
+    {
+        try
+        {
+            var logs = await new Logger().GetAllAsync(); 
+
+            dataGridView.Rows.Clear();
+        
+            foreach (var log in logs)
+            {
+                dataGridView.Rows.Add(
+                    log.Id,
+                    log.CreatedAt,
+                    log.UserId.HasValue ? log.UserId.Value.ToString() : "",
+                    log.Message,
+                    log.LogType
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Внутренняя ошибка при заполнени таблицы");
+        }
+    }
+}
