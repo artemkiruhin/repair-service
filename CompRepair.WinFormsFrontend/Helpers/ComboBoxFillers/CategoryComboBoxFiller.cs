3aaa768e3aae0adb64e498c7813d939a9e35de6b
artemkiruhin <temkaunik@gmail.com>
2025-01-19 12:00:30 +0300

    Category combo-box filler

---
+++ CompRepair.WinFormsFrontend/Helpers/ComboBoxFillers/CategoryComboBoxFiller.cs
@@ -0,0 +1,13 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.ComboBoxFillers;
+
+public class CategoryComboBoxFiller
+{
+    public async Task FillComboBox(ComboBox comboBox)
+    {
+        var categories = await new CategoryService().GetAllAsync();
+        comboBox.Items.Clear();
+        categories.ForEach(category => comboBox.Items.Add(category.Name));
+    }
+}
