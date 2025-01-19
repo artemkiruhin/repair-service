66c1f39129d4f33c83a2923d6ce1722c6da91f8c
artemkiruhin <temkaunik@gmail.com>
2025-01-19 09:08:56 +0300

    Used part filler for dgv

---
+++ CompRepair.WinFormsFrontend/Helpers/Fillers/UsedPartFiller.cs
@@ -0,0 +1,33 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+public class UsedPartFiller
+{
+    
+    public async Task FillDataGridView(DataGridView dataGridView)
+    {
+        try
+        {
+            var usedParts = await new UsedPartService().GetAllAsync(); 
+
+            dataGridView.Rows.Clear();
+        
+            foreach (var usedPart in usedParts)
+            {
+                dataGridView.Rows.Add(
+                    usedPart.Id,
+                    usedPart.Quantity,
+                    usedPart.PricePerPart,
+                    usedPart.PartId,
+                    usedPart.PartName,
+                    usedPart.RepairOrderId
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Внутренняя ошибка при заполнени таблицы");
+        }
+    }
+}
