c18e116a3aee5b87526d93d4dbd8f01a445809f1
artemkiruhin <temkaunik@gmail.com>
2025-01-19 11:56:44 +0300

    Log type filler from combobox

---
+++ CompRepair.WinFormsFrontend/Helpers/ComboBoxFillers/LogTypeFiller.cs
@@ -0,0 +1,15 @@
+﻿using CompRepair.Core.Models.Entities;
+
+namespace CompRepair.WinFormsFrontend.Helpers.ComboBoxFillers;
+
+public class LogTypeFiller
+{
+    public void FillComboBox(ComboBox comboBox)
+    {
+        comboBox.Items.Clear();
+        
+        comboBox.Items.Add(nameof(LogType.Info));
+        comboBox.Items.Add(nameof(LogType.Warn));
+        comboBox.Items.Add(nameof(LogType.Error));
+    }
+}
