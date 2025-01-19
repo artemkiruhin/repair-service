dfcd307063c36fc3c5620a61a7327019fbd8424d
artemkiruhin <temkaunik@gmail.com>
2025-01-19 09:11:01 +0300

    User filler for dgv

---
+++ CompRepair.WinFormsFrontend/Helpers/Fillers/UserFiller.cs
@@ -0,0 +1,37 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+public class UserFiller
+{
+    
+    public async Task FillDataGridView(DataGridView dataGridView)
+    {
+        try
+        {
+            var users = await new UserService().GetAllAsync(); 
+
+            dataGridView.Rows.Clear();
+        
+            foreach (var user in users)
+            {
+                dataGridView.Rows.Add(
+                    user.Id,
+                    user.Username,
+                    user.FullName,
+                    user.Email,
+                    user.Phone,
+                    user.Salary,
+                    user.HiredDate,
+                    user.FiredDate,
+                    user.BirthDate,
+                    user.Role
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Внутренняя ошибка при заполнени таблицы");
+        }
+    }
+}
