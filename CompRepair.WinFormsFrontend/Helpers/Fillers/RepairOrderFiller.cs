09af9edb0d9be08cb3c4045ac0a4008c05a2f8a0
artemkiruhin <temkaunik@gmail.com>
2025-01-19 09:06:56 +0300

    Repair order filler dgv

---
+++ CompRepair.WinFormsFrontend/Helpers/Fillers/RepairOrderFiller.cs
@@ -0,0 +1,43 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+public class RepairOrderFiller
+{
+    
+    public async Task FillDataGridView(DataGridView dataGridView)
+    {
+        try
+        {
+            var orders = await new RepairOrderService().GetAllAsync(); 
+
+            dataGridView.Rows.Clear();
+        
+            foreach (var order in orders)
+            {
+                dataGridView.Rows.Add(
+                    order.SerialNumber,
+                    order.ClientFullName,
+                    order.ClientPhone,
+                    order.ClientEmail,
+                    order.TechnicianId,
+                    order.TechnicianFullName,
+                    order.TechnicianPhone,
+                    order.TechnicianEmail,
+                    order.TechnicianUsername,
+                    order.DeviceTypeName,
+                    order.Problem,
+                    order.Diagnosis,
+                    order.Status,
+                    order.EstimatedCost,
+                    order.FinalCost ?? -1,
+                    order.CompletedAt.HasValue ? order.CompletedAt.Value.ToShortDateString() : "" 
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Внутренняя ошибка при заполнени таблицы");
+        }
+    }
+}
