9f24815da5b6b2000d9befc3d925a2ca3926b6f4
artemkiruhin <temkaunik@gmail.com>
2025-01-20 12:51:53 +0300

    Client combo box filler

---
+++ CompRepair.WinFormsFrontend/Helpers/ComboBoxFillers/ClientComboBoxFiller.cs
@@ -0,0 +1,13 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.ComboBoxFillers;
+
+public class ClientComboBoxFiller
+{
+    public async Task FillComboBox(ComboBox comboBox)
+    {
+        var clients = await new ClientService().GetAllAsync();
+        comboBox.Items.Clear();
+        clients.ForEach(client => comboBox.Items.Add($"{client.FullName} - {client.Phone}"));
+    }
+}
