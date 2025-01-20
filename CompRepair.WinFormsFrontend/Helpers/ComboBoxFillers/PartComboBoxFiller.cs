88c316d26478b459d3827e5f6a7e951c373e4307
artemkiruhin <temkaunik@gmail.com>
2025-01-20 12:47:39 +0300

    Part combo box filler

---
+++ CompRepair.WinFormsFrontend/Helpers/ComboBoxFillers/PartComboBoxFiller.cs
@@ -0,0 +1,13 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.ComboBoxFillers;
+
+public class PartComboBoxFiller
+{
+    public async Task FillComboBox(ComboBox comboBox)
+    {
+        var parts = await new PartService().GetAllAsync();
+        comboBox.Items.Clear();
+        parts.ForEach(part => comboBox.Items.Add(part.Name));
+    }
+}
