f1d855ef854047ea7caf412858f16a58a6782b04
artemkiruhin <temkaunik@gmail.com>
2025-01-20 12:48:48 +0300

    Order status combo box filler

---
+++ CompRepair.WinFormsFrontend/Helpers/ComboBoxFillers/OrderStatusComboBoxFiller.cs
@@ -0,0 +1,13 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.ComboBoxFillers;
+
+public class OrderStatusComboBoxFiller
+{
+    public async Task FillComboBox(ComboBox comboBox)
+    {
+        var statuses = await new OrderStatusService().GetAllAsync();
+        comboBox.Items.Clear();
+        statuses.ForEach(status => comboBox.Items.Add(status.Name));
+    }
+}
