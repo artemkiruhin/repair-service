a01f1fe8fb5d645954fe4c30547937f05c790c47
artemkiruhin <temkaunik@gmail.com>
2025-01-20 12:46:33 +0300

    Device type combo box filler

---
+++ CompRepair.WinFormsFrontend/Helpers/ComboBoxFillers/DeviceTypeComboBoxFiller.cs
@@ -0,0 +1,13 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.ComboBoxFillers;
+
+public class DeviceTypeComboBoxFiller
+{
+    public async Task FillComboBox(ComboBox comboBox)
+    {
+        var types = await new DeviceTypeService().GetAllAsync();
+        comboBox.Items.Clear();
+        types.ForEach(type => comboBox.Items.Add(type.Name));
+    }
+}
