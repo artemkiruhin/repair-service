d6136a438eefc3fcee622ca1b244071cbab01caf
artemkiruhin <temkaunik@gmail.com>
2025-01-20 12:49:48 +0300

    Manufacturer combo box filler

---
+++ CompRepair.WinFormsFrontend/Helpers/ComboBoxFillers/ManufacturerComboBoxFiller.cs
@@ -0,0 +1,14 @@
+﻿using CompRepair.Core.Services.EntityServices;
+using CompRepair.WinFormsFrontend.Helpers.Fillers;
+
+namespace CompRepair.WinFormsFrontend.Helpers.ComboBoxFillers;
+
+public class ManufacturerComboBoxFiller
+{
+    public async Task FillComboBox(ComboBox comboBox)
+    {
+        var manufacturers = await new ManufacturerService().GetAllAsync();
+        comboBox.Items.Clear();
+        manufacturers.ForEach(manufacturer => comboBox.Items.Add(manufacturer.Name));
+    }
+}
