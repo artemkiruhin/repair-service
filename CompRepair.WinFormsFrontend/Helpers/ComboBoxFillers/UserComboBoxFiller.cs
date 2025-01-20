0994131e1f1eec1f67dee3ccf2b1dbc9663583e2
artemkiruhin <temkaunik@gmail.com>
2025-01-20 12:43:32 +0300

    User combo box filler

---
+++ CompRepair.WinFormsFrontend/Helpers/ComboBoxFillers/UserComboBoxFiller.cs
@@ -0,0 +1,13 @@
+﻿using CompRepair.Core.Services.EntityServices;
+
+namespace CompRepair.WinFormsFrontend.Helpers.ComboBoxFillers;
+
+public class UserComboBoxFiller
+{
+    public async Task FillComboBox(ComboBox comboBox)
+    {
+        var users = await new UserService().GetAllAsync();
+        comboBox.Items.Clear();
+        users.ForEach(user => comboBox.Items.Add(user.Username));
+    }
+}
