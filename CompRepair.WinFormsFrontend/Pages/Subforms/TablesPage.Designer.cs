07d485dd76309fef6f9e0fd33933235569c21bf1
artemkiruhin <temkaunik@gmail.com>
2025-01-21 19:22:23 +0300

    Tables page created

---
+++ CompRepair.WinFormsFrontend/Pages/Subforms/TablesPage.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace CompRepair.WinFormsFrontend.Pages.Subforms
+{
+    partial class TablesPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(9, 9);
+            button1.Margin = new Padding(0);
+            button1.Name = "button1";
+            button1.Size = new Size(360, 185);
+            button1.TabIndex = 0;
+            button1.Text = "Категории";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button2.Location = new Point(369, 9);
+            button2.Margin = new Padding(0);
+            button2.Name = "button2";
+            button2.Size = new Size(360, 185);
+            button2.TabIndex = 1;
+            button2.Text = "Производители";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button3.Location = new Point(729, 9);
+            button3.Margin = new Padding(0);
+            button3.Name = "button3";
+            button3.Size = new Size(360, 185);
+            button3.TabIndex = 2;
+            button3.Text = "Виды девайсов";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button4.Location = new Point(9, 194);
+            button4.Margin = new Padding(0);
+            button4.Name = "button4";
+            button4.Size = new Size(360, 185);
+            button4.TabIndex = 3;
+            button4.Text = "Запчасти";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button5.Location = new Point(369, 194);
+            button5.Margin = new Padding(0);
+            button5.Name = "button5";
+            button5.Size = new Size(360, 185);
+            button5.TabIndex = 4;
+            button5.Text = "Использованные запчасти";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button6.Location = new Point(729, 194);
+            button6.Margin = new Padding(0);
+            button6.Name = "button6";
+            button6.Size = new Size(360, 185);
+            button6.TabIndex = 5;
+            button6.Text = "Заказы";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button7.Location = new Point(9, 379);
+            button7.Margin = new Padding(0);
+            button7.Name = "button7";
+            button7.Size = new Size(360, 185);
+            button7.TabIndex = 6;
+            button7.Text = "Клиенты";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button8.Location = new Point(369, 379);
+            button8.Margin = new Padding(0);
+            button8.Name = "button8";
+            button8.Size = new Size(360, 185);
+            button8.TabIndex = 7;
+            button8.Text = "Сотрудники";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // TablesPage
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1567, 825);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "TablesPage";
+            Text = "TablesPage";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+    }
+}
